b3a39f9305bfd2cf98b0fa335e6931d9fb260168
Loren Hayden <lorenhayden@hotmail.com>
2023-11-07 10:03:59 -0800

    Skills page updates

--- src/Skills.tsx
+++ src/Skills.tsx
@@ -1,5 +1,5 @@
 /* imports */
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { useTheme } from './components/ThemePicker';
 import skills from './data/skills.json';
 
@@ -8,6 +8,13 @@ import ProgressBar from './components/Progress';
 
 /* sass */
 import "./sass/_skills.scss";
+
+/* intersect observer config */
+const observerConfig = {
+  root: document.getElementById("app-content"),
+  threshold: 0.5,
+  rootMargin: "70px"
+}
 
 type SkillProp = {
   name: string,
@@ -22,6 +29,39 @@ type CategoryProp = {
 
 const Skills: FC = () => {
   const theme = useTheme();
+  const onObserve = (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+    if (entries && observer) {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.remove('fade-out');
+          entry.target.classList.add('fade-in');
+        } else {
+          entry.target.classList.remove('fade-in');
+          entry.target.classList.add('fade-out');
+        }
+      })
+    }
+  }
+  useEffect(() => {
+    const observer = new IntersectionObserver(onObserve, observerConfig);
+    const percentElements = document.querySelectorAll(`.progress-${theme.name}`)
+    if (observer && percentElements) {
+      percentElements.forEach(percentElement => {
+        observer.observe(percentElement)
+      })
+    }
+    const categoryElements = document.querySelectorAll(`.skill-tile-category-${theme.name}`)
+    if (observer && categoryElements) {
+      categoryElements.forEach(categoryElement => {
+        observer.observe(categoryElement)
+      })
+    }
+    return () => {
+      if (observer) {
+        observer.disconnect()
+      }
+    }
+  }, [])
   return (
     <div className={`skills-${theme.name}`}>
       <h1 className={`skills-header-${theme.name}`}>
@@ -31,8 +71,8 @@ const Skills: FC = () => {
         {skills.map((category: CategoryProp, categoryIndex: number) => {
           const categoryKey = `category-${categoryIndex}`
           return (
-            <div key={categoryKey} className={`skill-tile-${theme.name}`}>
-              <h2 className={`skill-tile-category-${theme.name}`}>
+            <div key={categoryKey} className={`skill-tile-${theme.name} fade-in`}>
+              <h2 className={`skill-tile-category-${theme.name} fade-in`}>
                 {category.name}
               </h2>
               <ul className={`skill-tile-skills-${theme.name}`}>
